b3898bedb6b79d8b812ec6d89051bfaf4b201af6
k-h-08 <kylehd08@gmail.com>
2025-04-21 21:37:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function runCode() {
-  const code = document.getElementById('editor').value;
-  document.getElementById('output').innerHTML = code;
-}
+// Future interactive scripts can go here
+console.log("Welcome to the Learn to Code platform!");
+
+// Add animated typing or lesson intro loader later
